c27aa18fcc3ad7c76826466067d5d0fbd57c40a0
Michael Utz <michael@theutz.com>
2017-02-02 20:42:04 -0600

    Setup home routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,15 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
-import { AppComponent } from './app.component';
-import {authConfig, firebaseConfig} from "environments/firebaseConfig";
-import {AngularFireModule} from "angularfire2";
-import {AuthService} from "app/shared/auth.service";
-import {LoginUserComponent} from "app/login-user/login-user.component";
-import {DisplayUserComponent} from "app/display-user/display-user.component";
-import { RoutingModule } from 'app/routing/routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { AngularFireModule } from 'angularfire2';
+import { DisplayUserComponent } from 'app/display-user/display-user.component';
+import { LoginUserComponent } from 'app/login-user/login-user.component';
 import { MarketingModule } from 'app/marketing/marketing.module';
+import { RoutingModule } from 'app/routing/routing.module';
+import { AuthService } from 'app/shared/auth.service';
+import { authConfig, firebaseConfig } from 'environments/firebaseConfig';
 
 @NgModule({
   declarations: [
--- src/app/routing/routing.module.ts
+++ src/app/routing/routing.module.ts
@@ -1,10 +1,19 @@
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from 'app/marketing/home/home.component';
+
+export const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  declarations: []
+  declarations: [],
+  exports: [RouterModule]
 })
 export class RoutingModule { }
