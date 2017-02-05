f27697d82f0e19878b3f63a31d65b632b4e8a1b3
Michael Utz <michael@theutz.com>
2017-02-05 12:21:50 -0600

    Created search box component

--- src/app/hotels/hotels.module.ts
+++ src/app/hotels/hotels.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { HotelRoutingModule } from 'app/hotels/hotel-routing/hotel-routing.module';
 import { HotelsComponent } from './hotels/hotels.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   imports: [
@@ -11,7 +12,8 @@ import { HotelsComponent } from './hotels/hotels.component';
   ],
   declarations: [
     HomeComponent,
-    HotelsComponent
+    HotelsComponent,
+    SearchComponent
   ]
 })
 export class HotelsModule { }
