08ec1966367ff1d1b90c32a098600c764004e2e0
Michael Utz <michael@theutz.com>
2017-02-05 21:43:50 -0600

    Tests passed

--- src/app/hotels/hotel-detail/hotel-detail.component.spec.ts
+++ src/app/hotels/hotel-detail/hotel-detail.component.spec.ts
@@ -1,3 +1,7 @@
+import { Subject } from 'rxjs/Rx';
+import { Observable } from '@angular-cli/ast-tools/node_modules/rxjs/Rx';
+import { ActivatedRoute } from '@angular/router';
+import { HotelsService } from '../../shared/hotels.service';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -5,15 +9,27 @@ import { DebugElement } from '@angular/core';
 
 import { HotelDetailComponent } from './hotel-detail.component';
 
+class HotelsServiceSpy {
+
+}
+
+class ActivatedRouteSpy {
+  data = new Subject<{ [name: string]: any }>().asObservable();
+}
+
 describe('HotelDetailComponent', () => {
   let component: HotelDetailComponent;
   let fixture: ComponentFixture<HotelDetailComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HotelDetailComponent ]
+      declarations: [HotelDetailComponent],
+      providers: [
+        { provide: HotelsService, useClass: HotelsServiceSpy },
+        { provide: ActivatedRoute, useClass: ActivatedRouteSpy }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
