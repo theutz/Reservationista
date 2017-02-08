122db0a12a78098149ae78bb2c4ae11906a797a1
Michael Utz <michael@theutz.com>
2017-02-07 21:03:13 -0600

    Tests passed

--- src/app/hotels/hotel-list/hotel-list.component.spec.ts
+++ src/app/hotels/hotel-list/hotel-list.component.spec.ts
@@ -1,6 +1,8 @@
+import { Subject } from 'rxjs/Rx';
+import { NgSpinKitModule } from 'ng-spin-kit/dist/spinners';
 import { Router } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { HotelsService } from '../../shared/hotels.service';
+import { Hotels, HotelsService } from '../../shared/hotels.service';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
@@ -9,8 +11,8 @@ import { DebugElement } from '@angular/core';
 import { HotelListComponent } from './hotel-list.component';
 
 class HotelsServiceSpy {
-  getAll = jasmine.createSpy('getAll').and.callFake(() => {
-  })
+  getAll = jasmine.createSpy('getAll')
+    .and.returnValue(new Subject<Hotels>().asObservable());
 }
 
 describe('HotelListComponent', () => {
@@ -19,6 +21,7 @@ describe('HotelListComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [NgSpinKitModule],
       declarations: [HotelListComponent],
       providers: [
         { provide: HotelsService, useClass: HotelsServiceSpy },
