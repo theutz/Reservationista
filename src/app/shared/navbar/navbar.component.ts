357f917a7cf5a9f041c183975384fe29a349a128
Michael Utz <michael@theutz.com>
2017-02-22 18:55:42 -0800

    Setup collapsable navbar

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -5,6 +5,7 @@ import { AuthService } from '../auth.service';
 import { BrandingService } from '../branding.service';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { CollapseDirective } from 'ng2-bootstrap';
 
 @Component({
   selector: 'app-navbar',
@@ -13,6 +14,7 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   title: string = '';
+  isCollapsed: boolean = true;
 
   constructor(
     private _brandingService: BrandingService,
@@ -23,6 +25,10 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
     this._setTitle();
+  }
+
+  toggleNavbar(): void {
+    this.isCollapsed = !this.isCollapsed;
   }
 
   isLoggedIn(): Observable<boolean> {
