7d219705debe578ebfde9dcb2834d9deac079b8c
Michael Utz <michaelutz@utzMBP.local>
2017-03-07 16:55:20 -0600

    Added link to navbar

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from '../shared/shared.module';
 import { CanDeactivateGuard } from '../shared/can-deactivate-guard.service';
 import { AdminRoutingModule } from './admin-routing/admin-routing.module';
 import { NgModule } from '@angular/core';
@@ -8,7 +9,8 @@ import { AdminComponent } from './admin/admin.component';
 @NgModule({
   imports: [
     CommonModule,
-    AdminRoutingModule
+    AdminRoutingModule,
+    SharedModule
   ],
   declarations: [
     AdminHomeComponent,
